042f4cc9780154e321087705766468468d8dc520
Vivek Jadhav <85949907+Vivek-Jadhav27@users.noreply.github.com>
2024-01-15 17:20:20 +0530

    Create videopage.dart

---
+++ lib/screens/videopage.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class VideoPage extends StatefulWidget {
+  const VideoPage({super.key});
+
+  @override
+  State<VideoPage> createState() => _VideoPageState();
+}
+
+class _VideoPageState extends State<VideoPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBody: true,
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text("Bottom Navigation Bar"),
+      ),
+    );
+  }
+}
